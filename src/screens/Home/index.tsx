2246437e8d0abbca62f4473b4a01e40aa9bfaa82
Herverson de Sousa <herverson.sousa@gmail.com>
2022-03-16 01:43:22 -0300

    add header and assets

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import { Container, Title } from './styles'
+import React from "react";
+import { StatusBar } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+import Logo from "../../assets/logo.svg";
+
+import { Container, Header, HeaderContent, TotalCars } from "./styles";
 
 export default function Home() {
   return (
     <Container>
-      <Title>Home</Title>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <TotalCars>Total de carros</TotalCars>
+        </HeaderContent>
+      </Header>
     </Container>
-  )
+  );
 }
