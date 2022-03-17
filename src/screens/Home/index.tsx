efce798b34a60954b2f70ae39a12da416c2fd92c
Herverson de Sousa <herverson.sousa@gmail.com>
2022-03-17 02:58:15 -0300

    add list car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,10 +3,21 @@ import { StatusBar } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 
 import Logo from "../../assets/logo.svg";
+import { Car } from "../../components/Car";
 
-import { Container, Header, HeaderContent, TotalCars } from "./styles";
+import { CarList, Container, Header, HeaderContent, TotalCars } from "./styles";
 
 export default function Home() {
+  const carData = {
+    brand: "Mercedes",
+    name: "C300",
+    rent: {
+      period: "AO DIA",
+      price: 200,
+    },
+    thumbnail:
+      "https://cdn.autopapo.com.br/box/uploads/2019/07/25111304/frente-mercedes-classe-c-cabriolet.png",
+  };
   return (
     <Container>
       <StatusBar
@@ -20,6 +31,12 @@ export default function Home() {
           <TotalCars>Total de carros</TotalCars>
         </HeaderContent>
       </Header>
+
+      <CarList
+        data={[1,2,3,4,5]}
+        keyExtractor={item => String(item)}
+        renderItem={({ item }) => <Car data={carData} />}
+      />
     </Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,11 +1,9 @@
+import { FlatList } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  /* justify-content: center;
-  align-items: center; */
-
   background-color: ${({ theme }) => theme.colors.background_primary};
 `;
 
@@ -31,3 +29,10 @@ export const TotalCars = styled.Text`
   font-family: ${({ theme }) => theme.fonts.primary_400};
   color: ${({ theme }) => theme.colors.text};
 `;
+
+export const CarList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 24
+  },
+  showsVerticalScrollIndicator: false
+})``;
