75ab944bd79bbfe824f5d274e2dac2c505692089
Herverson de Sousa <herverson.sousa@gmail.com>
2023-02-04 19:17:54 -0300

    splash screen

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -7,6 +7,7 @@ import { Scheduling } from '../screens/Scheduling'
 import { SchedulingDetails } from '../screens/SchedulingDetails';
 import { SchedulingComplete } from '../screens/SchedulingComplete';
 import { MyCars } from '../screens/MyCars';
+import { Splash } from '../screens/Splash';
 
 
 const { Navigator, Screen } = createNativeStackNavigator();
@@ -17,8 +18,9 @@ export function StackRoutes() {
       screenOptions={{
         headerShown: false,
       }}
-      initialRouteName="Home"
+      initialRouteName="Splash"
     >
+      <Screen name="Splash" component={Splash} />
       <Screen name="Home" component={Home} />
       <Screen name="CarDetails" component={CarDetails} />
       <Screen name="Scheduling" component={Scheduling} />
